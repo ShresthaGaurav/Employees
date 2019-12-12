e0c3c1d4d519d07dfb7cfbc4406a5905a2c69a1f
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-12-12 17:15:54 +0545

    search  with api

--- app/src/main/java/com/shresthagaurav/employees/UniqueSearch.java
+++ app/src/main/java/com/shresthagaurav/employees/UniqueSearch.java
@@ -1,18 +1,29 @@
 package com.shresthagaurav.employees;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.shresthagaurav.employees.api.EmployeeAPI;
+import com.shresthagaurav.employees.model.Employee;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class UniqueSearch extends AppCompatActivity {
     SearchView searchView;
-    RecyclerView recyclerView;
-
+    TextView textView;
+    private static String base_url = "http://dummy.restapiexample.com/api/v1/";
 
 
     @Override
@@ -20,8 +31,47 @@ public class UniqueSearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unique_search);
         searchView = findViewById(R.id.txt_search);
-        recyclerView = findViewById(R.id.RVunique);
+        textView = findViewById(R.id.search);
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Retrofit retrofit = new Retrofit.Builder().baseUrl(base_url)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+                final Call<Employee> listCall = employeeAPI.getEmployeeByID(Integer.parseInt(query));
+                listCall.enqueue(new Callback<Employee>() {
+                    @Override
+                    public void onResponse(Call<Employee> call, Response<Employee> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(UniqueSearch.this, "error"+response.code(), Toast.LENGTH_SHORT).show();
+                            Log.d("error","error" +response.code() );
+                            return;
+                        }
+                       String contex="";
+                        contex="id: "+response.body().getId()+
+                                "\n"+" name: "+response.body().getEmployee_name()+
+                                "\n"+" Salary: "+response.body().getEmployee_salary()
+                                +"\n"+" Age: "+response.body().getEmployee_age();
+                        textView.setText(contex);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Employee> call, Throwable t) {
+                        Toast.makeText(UniqueSearch.this, "error"+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("error","error   "+t.getLocalizedMessage() );
+
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
 
